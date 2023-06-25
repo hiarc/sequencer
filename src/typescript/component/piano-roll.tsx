f3f5fcc59893dc0fa6651c6ec107a2e8ea951585
hiarc <hshiro87@gmail.com>
2023-06-26 00:10:09 +0900

    ピアノロールの垂直線の実装

--- src/typescript/component/piano-roll.tsx
+++ src/typescript/component/piano-roll.tsx
@@ -6,12 +6,28 @@ export const PianoRoll: React.FunctionComponent<{notes: Message[]}> = (props) =>
       <div className="piano-roll" {...props}>
         <svg xmlns="http://www.w3.org/2000/svg" id="piano-roll" width="1240" height="1240" onClick={onClick}>
         {
+          // 平行線の描画。
+          // 1オクターブ（12音）毎に赤線で区切る。
           (function(){
             const paths = [];
             for(let i = 1; i <= 60; i++){
               const direction = `M 0 ${i * 15} L 1240 ${i * 15}`;
               const color = i % 12 === 0 ? "red" : "gray";
               const path = <path d={direction} stroke={color} strokeWidth={1} key={"pianoroll-horizon" + i}/>;
+              paths.push(path);
+            }
+             return paths;
+          }())
+        }
+        {
+          // 垂直線の描画。
+          // 8分音符毎に白線で区切る。
+          (function(){
+            const paths = [];
+            for(let i = 1; i <= 60; i++){
+              const direction = `M ${i * 50} 0 L ${i * 50} 1240`;
+              const color = i % 8 === 0 ? "white" : "gray";
+              const path = <path d={direction} stroke={color} strokeWidth={1} key={"pianoroll-vertical" + i}/>;
               paths.push(path);
             }
              return paths;
