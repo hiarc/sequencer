67c0feff98daa389ce346d64641432b078cf4b25
hiarc <hshiro87@gmail.com>
2023-07-01 16:08:54 +0900

    Pythonのコメントのルールに従ってフォーマットを修正した

--- src/python/domain/ports.py
+++ src/python/domain/ports.py
@@ -6,11 +6,12 @@ class Ports:
     def get_output_ports():
         return mido.get_output_names()
 
-    # 指定したMIDI出力ポートをオープンして返却する。
-    # ポートを指定していない場合や、指定したポートが利用できない場合は、最初に検出されたポートを開放して返却する。
-    # 利用可能なポートが存在しない場合や、指定したポートが利用できない場合は例外を発生させる。
     @staticmethod
     def open_output_port(port_name: str):
+        """指定したMIDI出力ポートをオープンして返却する。
+        ポートを指定していない場合や、指定したポートが利用できない場合は、最初に検出されたポートを開放して返却する。
+        利用可能なポートが存在しない場合や、指定したポートが利用できない場合は例外を発生させる。
+        """
         ports = mido.get_output_names()
         if port_name in ports:
             # TODO: 開いたポートを自動的に閉じるようにする
